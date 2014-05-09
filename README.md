ab1e8c64cb3a7077fc06865a943dfffc10e88685
Rudolf Potucek <rpotucek@rppower.local>
2014-05-09 16:24:58 -0600

    updated collector and documentation

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 TCVisualizer
 ============
 
-TeamCity build pipeline visualization
+TeamCity build pipeline visualization. The assumption is that you have a set of build chains in TeamCity and you want to have a quick visual overview where the chain breaks.
+
+Additional assumptions are:
+
+* you have found a way to have the same build number on each configuration in the chain
+* the relevant build configurations have the words "build" or "deploy" in them
+
+Installation
+------------
+
+You will need a unixy box with bash, mysql server, python 2.7, xmlstarlet.
+
+Running
+-------
+
+Run a cron job like this:
+
+...
+bash collector.sh -o insert | mysql
+...
+
+to insert data into the database. 
--- bash-scripts/collector.sh
+++ bash-scripts/collector.sh
@@ -1,20 +1,114 @@
 #!/bin/bash
 
 TEAMCITY=ampcity.smarttech.inc
+OUT_FORM=json
+
+PROGNAME=`basename $0`
+PROGPATH=`dirname  $0`
+cd "$PROGPATH"
+PROGPATH=`pwd`
+
+Usage() {
+  [ -n "$1" ] && echo "$1"
+  cat <<EOT
+#
+# Query teamcity for information about all builds and output data
+# for status portal
+#
+# USAGE: $PROGNAME [options]
+#
+# Options
+#
+# -a url  TeamCity URL
+# -o format  Select output format. Options: csv,json,insert
+#
+EOT
+  exit 1
+}
+
+while getopts ha:o: opt; do
+  case $opt in
+    h) 
+      Usage
+      ;;
+    a) 
+      TEAMCITY="$OPTARG"
+      ;;
+    o) 
+      case "$OPTARG" in
+        csv|json|insert) 
+          OUT_FORM="$OPTARG"
+          ;;
+        *) 
+          Usage
+          ;;
+      esac
+      ;;
+    *)
+      Usage
+      ;;
+  esac
+done
+shift $(($OPTIND -1))
+
 [[ -z "$XMLSTARLET" && -x /usr/local/bin/xml ]] && XMLSTARLET=/usr/local/bin/xml
 [[ -z "$XMLSTARLET" && -x /opt/local/bin/xml ]] && XMLSTARLET=/opt/local/bin/xml
+[[ -z "$XMLSTARLET" ]] && Usage "Need XML starlet"
 
 # Get all builds
 curl -s -n -X GET  -H "Content-type: text/plain" "http://ampcity.smarttech.inc/httpAuth/app/rest/builds" \
 | $XMLSTARLET sel -t -m '//build' -v '@href' -n \
 | while read uri; do
     curl -s -n -X GET -H "Content-type: text/plain" "http://ampcity.smarttech.inc${uri}" \
-    | $XMLSTARLET sel -t -m '//build' -v 'concat(
-        @number,",",
-        buildType/@projectName,",",
-        buildType/@name,",",
-        startDate,",",
-        finishDate,",",
-        @status)' -n \
-    | egrep -i '(build|deploy)'
+    | (
+        case $OUT_FORM in
+          json)
+            $XMLSTARLET sel -t -m '//build' -v 'concat(
+              "{",
+              "|buildNumber| : |", @number,                "|,",
+              "|projectName| : |", buildType/@projectName, "|,",
+              "|taskName| : |",    buildType/@name,        "|,",
+              "|startDate| : |",   startDate,              "|,",
+              "|endDate| : |",     finishDate,             "|,",
+              "|taskStatus| : |",  @status,                "|",
+              "}")' -n \
+            | sed 's/|/"/g' \
+            | egrep -i ' (build|deploy)'
+            ;;
+          csv)
+            $XMLSTARLET sel -t -m '//build' -v 'concat(
+              "buildNumber,", @number,                ",",
+              "projectName,", buildType/@projectName, ",",
+              "taskName,",    buildType/@name,        ",",
+              "startDate,",   startDate,              ",",
+              "endDate,",     finishDate,             ",",
+              "taskStatus,",  @status,                ","
+              )' -n \
+            | sed 's/|/"/g' \
+            | egrep -i ' (build|deploy)'
+            ;;
+          insert)
+            $XMLSTARLET sel -t -m '//build' -v 'concat(
+              "INSERT INTO buildhistory (buildNumber,projectName,taskName,startDate,endDate,status) VALUES (",
+              "|", @number,                "|,",
+              "|", buildType/@projectName, "|,",
+              "|", buildType/@name,        "|,",
+              "|", startDate,              "|,",
+              "|", finishDate,             "|,",
+              "|", @status,                "|)",
+              " ON DUPLICATE KEY UPDATE ",
+              "taskName=|", buildType/@name, "|,",
+              "startDate=|", startDate, "|,",
+              "endDate=|", finishDate, "|,",
+              "status=|", @status, "|",
+              ";")' -n \
+            | sed 's/|/"/g; s/\(T[0-9]*\)-0[67]00/\1/g;' \
+            | egrep -i '[0-9][^"]*(build|deploy)'
+            
+            ;;
+          *)
+            Usage "Unkown outform $OUT_FORM"
+            ;;
+        esac
+      )
   done
